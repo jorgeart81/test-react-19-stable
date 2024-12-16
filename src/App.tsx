50e1bf7c223d92b7f91ddea43567e30965fc841d
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2024-12-15 20:09:53 -0500

    apply react 19 use

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { Suspense } from 'react';
+import { getPlanets } from './actions/get-planets.action';
 import Planets from './pages/Planets';
 
 function App() {
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Planetas del Sistema Solar</h1>
+    <div className='p-4'>
+      <h1 className='text-2xl font-bold mb-4'>Planetas del Sistema Solar</h1>
 
-      <Planets />
+      <Suspense fallback={<span>Loading...</span>}>
+        <Planets getPlanets={getPlanets()} />
+      </Suspense>
     </div>
   );
 }
