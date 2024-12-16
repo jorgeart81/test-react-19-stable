68b571b1916e575fa1cb6a1e447ef34eb1d6a051
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2024-12-15 20:38:00 -0500

    add ErrorBoundary.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { Suspense } from 'react';
+
 import { getPlanets } from './actions/get-planets.action';
 import Planets from './pages/Planets';
+import { ErrorBoundary } from './shared/ErrorBoundary';
 
 function App() {
   return (
     <div className='p-4'>
       <h1 className='text-2xl font-bold mb-4'>Planetas del Sistema Solar</h1>
 
-      <Suspense fallback={<span>Loading...</span>}>
-        <Planets getPlanets={getPlanets()} />
-      </Suspense>
+      <ErrorBoundary
+        fallback={<span className='text-red-500'>Error loading planets</span>}
+      >
+        <Suspense fallback={<span className='animate-pulse'>Loading...</span>}>
+          <Planets getPlanets={getPlanets()} />
+        </Suspense>
+      </ErrorBoundary>
     </div>
   );
 }
