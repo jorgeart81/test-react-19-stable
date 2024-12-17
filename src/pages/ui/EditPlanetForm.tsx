ee71178ea3f03f0f882228d4df2f7e0ae19afdd0
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2024-12-16 23:03:47 -0500

    add useActionState

--- src/pages/ui/EditPlanetForm.tsx
+++ src/pages/ui/EditPlanetForm.tsx
@@ -1,46 +1,52 @@
-import { useState } from 'react';
-import { Planet } from '../../interfaces/planet.interface';
+import { useActionState } from 'react';
+
+import type { Planet } from '../../interfaces/planet.interface';
+import { createPlanetAction } from '../../actions/create-planet.action';
 
 interface Props {
-  onAddPlanet: (planet: Partial<Planet>) => void;
+  onAddPlanet: (planet: Planet) => void;
 }
 
 export const EditPlanetForm = ({ onAddPlanet }: Props) => {
-  const [name, setName] = useState('');
-  const [type, setType] = useState('');
-  const [distanceFromSun, setDistanceFromSun] = useState('');
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [state, formAction, isPending] = useActionState(
+    async (prevState: unknown, queryData: FormData) => {
+      const formData: Partial<Planet> = Object.fromEntries(queryData);
+      const planet = await createPlanetAction(formData);
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    onAddPlanet({ name, type, distanceFromSun });
-  };
+      console.log({ prevState, queryData });
+      onAddPlanet(planet);
+    },
+    null
+  );
 
   return (
-    <form className="mb-4 flex flex-col md:flex-row" onSubmit={handleSubmit}>
+    <form className='mb-4 flex flex-col md:flex-row' action={formAction}>
       <input
-        type="text"
-        placeholder="Nombre del planeta"
-        className="mb-2 md:mb-0 md:mr-2 p-2 border border-gray-300 rounded flex-1"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
+        type='text'
+        name='name'
+        placeholder='Nombre del planeta'
+        className='mb-2 md:mb-0 md:mr-2 p-2 border border-gray-300 rounded flex-1'
+        required
       />
       <input
-        type="text"
-        placeholder="Tipo de astro"
-        className="mb-2 md:mb-0 md:mr-2 p-2 border border-gray-300 rounded flex-1"
-        value={type}
-        onChange={(e) => setType(e.target.value)}
+        type='text'
+        name='type'
+        placeholder='Tipo de astro'
+        className='mb-2 md:mb-0 md:mr-2 p-2 border border-gray-300 rounded flex-1'
+        required
       />
       <input
-        type="text"
-        placeholder="Distancia del sol"
-        className="mb-2 md:mb-0 md:mr-2 p-2 border border-gray-300 rounded flex-1"
-        value={distanceFromSun}
-        onChange={(e) => setDistanceFromSun(e.target.value)}
+        type='text'
+        name='distanceFromSun'
+        placeholder='Distancia del sol'
+        className='mb-2 md:mb-0 md:mr-2 p-2 border border-gray-300 rounded flex-1'
+        required
       />
       <button
-        type="submit"
-        className="bg-blue-500 text-white p-2 rounded flex-1 sm:flex-none"
+        type='submit'
+        className='bg-blue-500 disabled:bg-gray-500 text-white p-2 rounded flex-1 sm:flex-none'
+        disabled={isPending}
       >
         Agregar planeta
       </button>
